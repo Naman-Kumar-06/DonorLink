200516efcd084d88d157dadd584f7b11a9973e83
Naman-Kumar-06 <162883121+Naman-Kumar-06@users.noreply.github.com>
2025-04-13 22:25:59 +0530

    Update database.py

--- database.py
+++ database.py
@@ -1,7 +1,7 @@
 import os
 import firebase_admin
 from firebase_admin import credentials, firestore, auth as admin_auth
-import pyrebase4
+import pyrebase
 from datetime import datetime
 
 # Firebase configuration
